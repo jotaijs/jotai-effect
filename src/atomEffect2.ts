2a92b9668954edc1bd2dc85478cd582307799972
daishi <daishi@axlight.com>
2023-10-07 10:27:44 +0900

    update jotai

--- src/atomEffect2.ts
+++ src/atomEffect2.ts
@@ -1,19 +1,17 @@
 import type { Getter, Setter, WritableAtom } from 'jotai/vanilla'
 import { atom } from 'jotai/vanilla'
 
-type CleanupFn = () => PromiseOrValue<void>
 type PromiseOrValue<T> = Promise<T> | T
+type CleanupFn = () => PromiseOrValue<void>
 
 export function atomEffect(
   effectFn: (get: Getter, set: Setter) => PromiseOrValue<void | CleanupFn>
 ) {
   const refAtom = atom<{
     mounted: boolean
-    inProgress: number
     cleanup: CleanupFn | void
   }>(() => ({
     mounted: false,
-    inProgress: 0,
     cleanup: undefined,
   }))
 
@@ -39,25 +37,18 @@ export function atomEffect(
     async (get, { setSelf }) => {
       get(refreshAtom)
       const ref = get(refAtom)
-      if (!ref.mounted || ref.inProgress) {
+      if (!ref.mounted) {
         return
       }
       await ref.cleanup?.()
-      const cleanup = await effectFn(get, setSelf as Setter)
-      ref.cleanup = cleanup
+      ref.cleanup = await effectFn(get, setSelf as Setter)
     },
     (
-      get,
+      _get,
       set,
       a: WritableAtom<unknown, unknown[], unknown>,
       ...args: unknown[]
-    ) => {
-      const ref = get(refAtom)
-      ref.inProgress++
-      const result = set(a, ...args)
-      ref.inProgress--
-      return result
-    }
+    ) => set(a, ...args)
   )
 
   return atom((get) => {
