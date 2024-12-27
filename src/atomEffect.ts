58e6a0a3fb58c7d61b00a4e3528e2fe3e6158e7c
David Maskasky <dmaskasky@gmail.com>
2024-12-26 23:38:00 -0700

    sync effect with store.sub

--- src/atomEffect.ts
+++ src/atomEffect.ts
@@ -1,38 +1,68 @@
 import type { Atom, Getter, Setter } from 'jotai/vanilla'
 import { atom } from 'jotai/vanilla'
 
-type Cleanup = () => void
+type AnyAtom = Atom<unknown>
 type GetterWithPeek = Getter & { peek: Getter }
 type SetterWithRecurse = Setter & { recurse: Setter }
+type Cleanup = () => void
 export type Effect = (
   get: GetterWithPeek,
   set: SetterWithRecurse
 ) => void | Cleanup
-
 type Ref = {
   get: GetterWithPeek
   set?: SetterWithRecurse
+  peek?: Getter
   cleanup?: Cleanup | null
   fromCleanup: boolean
   inProgress: number
-  deps: Set<Atom<unknown>>
-  init: () => void
+  isPending: boolean
+  deps: Set<AnyAtom>
+  sub: () => () => void
+  epoch: number
 }
 
 export function atomEffect(effect: Effect) {
   const refAtom = atom(
-    () => ({ deps: new Set(), inProgress: 0 }) as Ref,
-    (get) => {
+    () => ({ deps: new Set(), inProgress: 0, epoch: 0 }) as Ref,
+    (get, set) => {
       const ref = get(refAtom)
+      if (!ref.get.peek) {
+        ref.get.peek = get
+      }
+      const setter: Setter = (a, ...args) => {
+        try {
+          ++ref.inProgress
+          return set(a, ...args)
+        } finally {
+          --ref.inProgress
+        }
+      }
+      const recurse: Setter = (a, ...args) => {
+        if (ref.fromCleanup) {
+          if (process.env?.MODE !== 'production') {
+            console.warn('cannot recurse inside cleanup')
+          }
+          return undefined as any
+        }
+        return set(a, ...args)
+      }
+      if (!ref.set) {
+        ref.set = Object.assign(setter, { recurse })
+      }
+      ref.isPending = ref.inProgress === 0
       return () => {
         ref.cleanup?.()
         ref.cleanup = null
+        ref.isPending = false
         ref.deps.clear()
       }
     }
   )
   refAtom.onMount = (mount) => mount()
+  const refreshAtom = atom(0)
   const internalAtom = atom((get) => {
+    get(refreshAtom)
     const ref = get(refAtom)
     if (!ref.get) {
       ref.get = ((a) => {
@@ -40,58 +70,45 @@ export function atomEffect(effect: Effect) {
         return get(a)
       }) as Getter & { peek: Getter }
     }
-    ref.init()
     ref.deps.forEach(get)
-    if (ref.inProgress > 0) {
-      return
-    }
-    ref.cleanup?.()
-    const cleanup = effectAtom.effect(ref.get!, ref.set!)
-    ref.cleanup =
-      typeof cleanup === 'function'
-        ? () => {
-            try {
-              ref.fromCleanup = true
-              cleanup()
-            } finally {
-              ref.fromCleanup = false
-            }
-          }
-        : null
+    ref.isPending = true
+    return ++ref.epoch
   })
-  internalAtom.unstable_onInit = (store) => {
-    const ref = store.get(refAtom)
-    const get = store.get
-    const set = store.set
-    ref.init = () => {
-      if (!ref.get.peek) {
-        ref.get.peek = get
-      }
-      if (!ref.set) {
-        const setter: Setter = (a, ...args) => {
-          try {
-            ++ref.inProgress
-            return set(a, ...args)
-          } finally {
-            --ref.inProgress
-            ref.get(a) // FIXME why do we need this?
-          }
+  const bridgeAtom = atom(
+    (get) => get(internalAtom),
+    (get, set) => {
+      set(refreshAtom, (v) => ++v)
+      return get(refAtom).sub()
+    }
+  )
+  bridgeAtom.onMount = (mount) => mount()
+  bridgeAtom.unstable_onInit = (store) => {
+    store.get(refAtom).sub = () => {
+      const listener = () => {
+        const ref = store.get(refAtom)
+        if (!ref.isPending || ref.inProgress > 0) {
+          return
         }
-        const recurse: Setter = (a, ...args) => {
-          if (ref.fromCleanup) {
-            if (process.env.NODE_ENV !== 'production') {
-              console.warn('cannot recurse inside cleanup')
-            }
-            return undefined as any
-          }
-          return set(a, ...args)
-        }
-        ref.set = Object.assign(setter, { recurse })
+        ref.isPending = false
+        ref.cleanup?.()
+        const cleanup = effectAtom.effect(ref.get!, ref.set!)
+        ref.cleanup =
+          typeof cleanup === 'function'
+            ? () => {
+                try {
+                  ref.fromCleanup = true
+                  cleanup()
+                } finally {
+                  ref.fromCleanup = false
+                }
+              }
+            : null
       }
+      return store.sub(internalAtom, listener, 'H')
     }
   }
   const effectAtom = Object.assign(
-    atom((get) => get(internalAtom)),
+    atom((get) => void get(bridgeAtom)),
     { effect }
   )
   return effectAtom
